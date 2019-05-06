537711806c28825bdeff054f8d04aedf17362c14
Luis Arpasi <meclipse@gmail.com>
2019-05-06 02:10:23 -0500

    Cleanup

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -3,29 +3,17 @@ import { Task } from '../models'
 // Controllers should not be coupled to express (req, res, next)
 export const TaskController = {
 	async saveTask({ title, description }) {
-		try {
-			const newTask = new Task({
-				title,
-				description
-			})
+		const newTask = new Task({
+			title,
+			description
+		})
 
-			return await newTask.save()
-		} catch (err) {
-			throw err
-		}
+		return await newTask.save()
 	},
 	async getAll() {
-		try {
-			return await Task.find({}).exec()
-		} catch (err) {
-			throw err
-		}
+		return await Task.find({}).exec()
 	},
 	async getById({ id }) {
-		try {
-			return await Task.findById(id)
-		} catch (err) {
-			throw err
-		}
+		return await Task.findById(id)
 	}
 }
